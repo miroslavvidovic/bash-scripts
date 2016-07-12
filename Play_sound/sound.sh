4111cb7524f8566ea978529b2ae952b21f789250
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-07-12 16:31:03 +0200

    Play a sound

---
+++ Play_sound/sound.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# -------------------------------------------------------
+# Info:
+# 	Miroslav Vidovic
+# 	sound.sh
+# 	15.06.2016.-08:21:23
+# -------------------------------------------------------
+# Description:
+#   Play a sound from terminal. Useful as and alert when
+#   a long task finishes.
+# Usage:
+#   sudo apt-get upgrade; bash sound.sh
+# Info:
+#   Could be moved to an alias.
+# -------------------------------------------------------
+# Script:
+
+paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+
+exit 0
