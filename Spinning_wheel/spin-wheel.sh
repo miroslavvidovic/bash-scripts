717fcdd08e464cbbc3659d188efd756521f2f5cc
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-08-27 19:10:59 +0200

    Spining animation for loading time

---
+++ Spinning_wheel/spin-wheel.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Info:
+# 	Miroslav Vidovic
+# 	spin-wheel.sh
+# 	27.08.2016.-12:10:47
+# -----------------------------------------------------------------------------
+# Description:
+#   Show the spinner animation while executing some long command
+# Usage:
+#   For this example:
+#   spin-wheel.sh sleep 10
+# -----------------------------------------------------------------------------
+# Script:
+
+show_spinner()
+{
+  # ID of the process the executing while the spinner is showing
+  local -r pid="${1}"
+  # Delay for the spinner animation
+  local -r delay='0.80'
+  # Lines forming the spinner
+  local spinstr='|/-\'
+  # Emtpy variable
+  local temp
+  # Disable the cursor for better animation
+  tput civis
+
+  # Show the animation while the process is running
+  while [ "$(ps a | awk '{print $1}' | grep -w $pid)" ]; do
+    temp="${spinstr#?}"
+    printf " [%c]  " "${spinstr}"
+    spinstr=${temp}${spinstr%"${temp}"}
+    sleep "${delay}"
+    printf "\b\b\b\b\b\b"
+  done
+  printf "    \b\b\b\b"
+  # Enable the cursor again
+  tput cnorm
+}
+
+# Trap the exit signal and enable cursor
+signal() {
+  echo -e "\n\nExiting on trapped signal...\n"
+  tput civis
+  exit 1
+}
+
+trap signal 1 2 3 15
+
+("$@") &
+show_spinner "$!"
+
+exit 0
