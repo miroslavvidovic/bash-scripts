040992945867c09b9720c03f8bcd7c4505905125
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2016-12-19 18:42:01 +0100

    Add info header to existing bash script

---
+++ Add_header/add_header.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Info:
+# 	Miroslav Vidovic
+# 	add_header.sh
+# 	19.12.2016.-12:27:34
+# -----------------------------------------------------------------------------
+# Description:
+#   Add the info header to an existing bash script.
+# Usage:
+#   add_header.sh script_name.sh
+# -----------------------------------------------------------------------------
+# Script:
+
+input_file=$1
+
+timestamp=$(date +"%d.%m.%Y.-%H:%M:%S")
+
+sed -i -e "1i#!/bin/bash\n\n# -------------------------------------------------------\n# Info:\n#   Miroslav Vidovic\n#   $input_file\n#   $timestamp\n# -------------------------------------------------------\n# Description:\n#\n# Usage:\n#   $input_file\n#\n#   Lorem ipsum usage\n# -------------------------------------------------------\n# Script:\n" $input_file
+
