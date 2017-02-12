38c035e9a586fa1758a465f03b9afa208359996c
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2017-02-12 18:57:32 +0100

    Fix header and shellcheck warnings.

--- Check_process/check_process.sh
+++ Check_process/check_process.sh
@@ -2,25 +2,29 @@
 
 # -----------------------------------------------------------------------------
 # Info:
-# 	Miroslav Vidovic
-# 	check_process.sh
-# 	19.12.2016.-15:59:03
+#   author:    Miroslav Vidovic
+#   file:      check_process.sh
+#   created:   19.12.2016.-15:59:03
+#   revision:  12.02.2017.
+#   version:   1.1
 # -----------------------------------------------------------------------------
+# Requirements:
+#
 # Description:
 #   Check if a process is running.
 # Usage:
-# 
+#   check_process.sh process_name
 # -----------------------------------------------------------------------------
 # Script:
 
-process=$1
+process="$1"
 
-# Check if gedit is running
+# Check if a proces is running
 if pgrep "$process" > /dev/null
 then
     echo "$process running"
     echo "----------------"
-    ps aux | grep $process | grep -v grep | grep -v $0
+    ps aux | grep "$process" | grep -v grep | grep -v "$0"
 else
     echo "$process not running"
 fi
