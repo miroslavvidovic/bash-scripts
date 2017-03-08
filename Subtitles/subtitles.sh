7e24518669d537614a6769c1c741e700d9ecc48d
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2017-03-08 17:45:28 +0100

    Bug fix for names with spaces

--- Subtitles/subtitles.sh
+++ Subtitles/subtitles.sh
@@ -2,16 +2,20 @@
 
 # -----------------------------------------------------------------------------
 # Info:
-# 	Miroslav Vidovic
-# 	subtitles.sh
-# 	14.10.2016.-20:58:57
+#   author:    Miroslav Vidovic
+#   file:      subtitles.sh
+#   created:   14.10.2016.-20:58:57
+#   revision:  08.03.2017.
+#   version:   1.1
 # -----------------------------------------------------------------------------
+# Requirements:
+#   subliminal
 # Description:
-#   A small wrapper for subliminal 
+#   A small wrapper script for subliminal
 #   https://github.com/Diaoul/subliminal
 # Usage:
-# subtitles.sh movie_name.mp4
-# 
+#   subtitles.sh movie_name.mp4
+#
 # -----------------------------------------------------------------------------
 # Script:
 
@@ -19,11 +23,11 @@ main(){
   # Check if subliminal is installed
   hash subliminal 2>/dev/null || { echo >&2 "I require subliminal but it's not installed.  Aborting."; exit 1; }
   # Check for empty input
-  if [[ -z $1 ]]; then
+  if [[ -z "$@" ]]; then
     echo "Input file needed."
   else
     # Download English and Serbian subtitles
-    subliminal download -l en -l srp $1
+    subliminal download -l en -l srp "$@"
     # Send notification
     notify-send "Subtitles.sh finished"
     # If sound.sh script is available use it to play a sound 
