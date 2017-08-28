f0f0b76181ff2094ff20339cd16c6665a1ced115
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2017-08-28 11:20:36 +0200

    Simple menu to run the script in a loop

--- random-movie/random-movie.sh
+++ random-movie/random-movie.sh
@@ -14,7 +14,7 @@
 #   Play a random movie from a directory. If you can't decide what to watch let
 #   this script do it for you.
 # Usage:
-#
+#   random-movie.sh
 # -----------------------------------------------------------------------------
 # Script:
 
@@ -27,6 +27,7 @@ main(){
   # Create a movie array with the result from find
   # find looks for all the files with the listed extensions
   # mkv, mp4, wmv, flv, webm, mov, avi
+  declare -a movie_array
   readarray -t movie_array <<< "$( find "$MOVIES_LOCATION" -type f -name '*.mkv' -o -name '*.avi' -o -name '*.mp4' -o -name '*.wmv' -o -name '*.flv' -o -name '*.webm' -o -name '*.mov')"
 
   # Number of items in movie_array
@@ -34,8 +35,34 @@ main(){
   max_random=$((num_of_movies - 1))
   # Create a random number between 0 and max_random
   random_movie_num=$(shuf -i 0-$max_random -n 1)
-  # Open the movie
-  "$PLAYER" "${movie_array[$random_movie_num]}" 2>/dev/null 1>/dev/null &
+  
+  selected_file="${movie_array[$random_movie_num]}"
+
+  while true; do
+    echo "
+    $(basename "$selected_file")
+
+    (p) - play
+    (r) - run random again
+    (e) - exit"
+
+    read -p "=> " command
+
+    case "$command" in
+      p )
+        # Open the movie
+        "$PLAYER" "$selected_file" 2>/dev/null 1>/dev/null &
+        break
+        ;;
+      e)
+        break
+        ;;
+      r)
+        bash "$0"
+        break
+        ;;
+    esac
+  done
 }
 
 main "$@"
